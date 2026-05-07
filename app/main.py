a00c4ee225517ca801d6ce69a8cc064277e2ba71
Mr-Usty <uasetechstudio@gmail.com>
2026-05-07 04:37:44 -0700

    fix 2

--- app/main.py
+++ app/main.py
@@ -1,10 +1,35 @@
 ﻿from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-print("MAIN.PY STARTING")
+print("STEP 1")
 
-app = FastAPI()
+from app.database.connection import connect_db, close_db
 
-print("FASTAPI CREATED")
+print("STEP 2")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("STEP 3 - CONNECTING DB")
+
+    try:
+        await connect_db()
+        print("STEP 4 - DB CONNECTED")
+    except Exception as e:
+        print("DB ERROR:", str(e))
+
+    yield
+
+    print("STEP 5 - CLOSING DB")
+
+    try:
+        await close_db()
+    except Exception as e:
+        print("CLOSE DB ERROR:", str(e))
+
+
+app = FastAPI(lifespan=lifespan)
+
+print("STEP 6")
 
 
 @app.get("/")
